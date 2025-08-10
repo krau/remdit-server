3dc1a129c1a90f71ea19ec5f8bdbb372ece03263
krau <71133316+krau@users.noreply.github.com>
2025-08-10 12:19:37 +0800

    feat: 增加文件扩展名到语言类型的映射

--- api/handler.go
+++ api/handler.go
@@ -118,7 +118,179 @@ func handleGetFile(c *fiber.Ctx) error {
 				return "plaintext"
 			}
 			ext = strings.TrimPrefix(ext, ".")
-			return ext
+			if lang, ok := extToLang[strings.ToLower(ext)]; ok {
+				return lang
+			}
+			return "plaintext"
 		}(),
 	})
+}
+
+var extToLang = map[string]string{
+	"abap": "abap",
+
+	"bat": "bat",
+	"cmd": "bat",
+
+	"bicep": "bicep",
+
+	"c": "c",
+
+	"mligo": "cameligo",
+
+	"clj":  "clojure",
+	"cljs": "clojure",
+	"cljc": "clojure",
+
+	"coffee": "coffeescript",
+	"iced":   "coffeescript",
+
+	"cpp": "cpp",
+	"cc":  "cpp",
+	"cxx": "cpp",
+	"hpp": "cpp",
+	"hh":  "cpp",
+
+	"cs": "csharp",
+
+	"css": "css",
+
+	"dart": "dart",
+
+	"Dockerfile": "dockerfile",
+	"dockerfile": "dockerfile",
+
+	"ecl": "ecl",
+
+	"ex":  "elixir",
+	"exs": "elixir",
+
+	"fs":  "fsharp",
+	"fsi": "fsharp",
+	"fsx": "fsharp",
+
+	"go": "go",
+
+	"graphql": "graphql",
+	"gql":     "graphql",
+
+	"hbs":        "handlebars",
+	"handlebars": "handlebars",
+
+	"hcl": "hcl",
+	"tf":  "hcl", // common for Terraform (HCL)
+
+	"html": "html",
+	"htm":  "html",
+
+	"ini": "ini",
+
+	"java": "java",
+
+	"js":  "javascript",
+	"mjs": "javascript",
+	"cjs": "javascript",
+	"jsx": "javascript",
+
+	"json":  "json",
+	"jsonc": "json",
+
+	"jl": "julia",
+
+	"kt":  "kotlin",
+	"kts": "kotlin",
+
+	"less": "less",
+
+	"liquid": "liquid",
+
+	"lua": "lua",
+
+	"md":       "markdown",
+	"markdown": "markdown",
+
+	"pas": "pascal",
+	"pp":  "pascal",
+
+	"pl": "perl",
+	"pm": "perl",
+
+	"php":   "php",
+	"phtml": "php",
+	"inc":   "php",
+
+	"txt": "plaintext",
+
+	"ps1":  "powershell",
+	"psm1": "powershell",
+	"psd1": "powershell",
+
+	"proto": "proto",
+
+	"pug":  "pug",
+	"jade": "pug",
+
+	"py":  "python",
+	"pyw": "python",
+	"pyi": "python",
+
+	"qs": "qsharp",
+
+	"r":   "r",
+	"rmd": "r",
+
+	"cshtml": "razor",
+	"vbhtml": "razor",
+
+	"rst": "restructuredtext",
+
+	"rb":   "ruby",
+	"erb":  "ruby",
+	"rake": "ruby",
+
+	"rs": "rust",
+
+	"scala": "scala",
+	"sc":    "scala", // ammonite scripts
+
+	"scm": "scheme",
+	"ss":  "scheme",
+
+	"scss": "scss",
+
+	"sh":   "shell",
+	"bash": "shell",
+
+	"sol": "sol",
+
+	"rq":     "sparql",
+	"sparql": "sparql",
+
+	"sql": "sql",
+
+	"st": "st",
+
+	"swift": "swift",
+
+	"sv":  "systemverilog",
+	"svh": "systemverilog",
+
+	"tcl": "tcl",
+
+	"twig": "twig",
+
+	"ts":  "typescript",
+	"tsx": "typescript",
+
+	"vb": "vb",
+
+	"v":  "verilog",
+	"vh": "verilog",
+
+	"xml": "xml",
+	"xsd": "xml",
+	"xsl": "xml",
+
+	"yaml": "yaml",
+	"yml":  "yaml",
 }
